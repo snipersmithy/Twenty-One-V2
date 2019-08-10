8d20f29bb072a81ab29420d689cda64f5b3e7e31
snipersmithy <53455394+snipersmithy@users.noreply.github.com>
2019-08-10 18:08:19 +0100

    Add files via upload

---
+++ Twenty One V2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardGameClassLibrary;
+
+namespace Twenty_One_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
